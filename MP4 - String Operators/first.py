c51a972e2bd6dde36928978c3c54b8c2827c7930
brent <rameadtnerbnamzuged@gmail.com>
2026-02-27 00:07:02 +0800

    Delete MP4 - String Operators/first.py

--- MP4 - String Operators/first.py
+++
@@ -1,9 +0,0 @@
-# Full name
-
-first = input("Enter your first name:\n").strip().capitalize()
-middle = input("Enter your middle name:\n").strip().capitalize()
-last = input("Enter your last name:\n").strip().capitalize()
-
-formatted_name = f"{last}, {first} {middle[0]}."
-
-print("Formatted Name:", formatted_name)
