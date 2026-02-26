4b01e0af53c4d54da128937a1e1cdcfa17a47e77
ChenzMV <gmocampo@mymail.mapua.edu.ph>
2026-02-26 22:01:10 +0800

    mp4_ocampo_glenpatrick.py

---
+++ MP4 - String Operators/mp4_ocampo_glenpatrick.py
@@ -0,0 +1,65 @@
+#1 Full Name Super Formatter (10 pts.)
+
+fname = input("Enter your first name: ").strip().capitalize()
+mname = input("Enter your middle name: ").strip().capitalize()
+lname = input("Enter your last name: ").strip().capitalize()
+
+print("Formatted Name: " + lname + ", " + fname + " " + mname[0] + "."
+
+
+#2
+      
+
+
+
+#3
+
+
+
+
+
+
+
+
+
+
+
+
+
+#4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#5
+
+
+
+
+
+
+
+
+#6
+
+
+
+
+
+
+
+
+
+
+
+#7
